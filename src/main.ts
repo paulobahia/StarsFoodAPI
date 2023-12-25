168d5a4b62a3c224ee8127eacc558f14bbd4d69c
Paulo Bahia <pauloipatinga22@gmail.com>
2023-12-25 01:31:05 -0300

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ credentials: true, origin: 'http://localhost:8000' })
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Stars Food API')
+    .setDescription('API para gerenciar os produtos e pedidos do seu restaurante.')
+    .setVersion('1.0')
+    .addTag('endpoints')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(5002);
+}
+bootstrap();
